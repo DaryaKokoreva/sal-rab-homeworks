e38c6e4c0b5adcdcf6cb1f7b856e2a783d7f5e47
KokorevaD <Yasenevad@gmail.com>
2023-01-21 21:22:57 +0300

    task 3 done_

--- task_3/solution.js
+++ task_3/solution.js
@@ -26,7 +26,11 @@ function renderCartItem(item) {
 
     let itemCountText = "";
     
-    itemCountText = itemCountText + product.count + " × " + product.price + " ₽ = ";
+    //itemCountText = itemCountText + product.count + " × " + product.price + " ₽ = ";
+    itemCountText = itemCountText + product.count;
+    itemCountText = itemCountText + " × ";
+    itemCountText = itemCountText + product.price;
+    itemCountText = itemCountText + " ₽ = ";
 
     let sum = product.count * product.price;
 
