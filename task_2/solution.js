b793a8105a2609218081341b88b8d9cca600deef
KokorevaD <Yasenevad@gmail.com>
2023-01-21 17:11:58 +0300

    task 2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -18,14 +18,11 @@ function calcShipping(sum, min, shipping) {
 
 
     let shippingSum;
-
-    if(productsSum == 0){
+    
+    if(productsSum == 0 || productsSum >= freeShippingMinSum){
     shippingSum = 0;
 }
-    else if(productsSum >= freeShippingMinSum){
-    shippingSum = 0;
-}
-    else(productsSum > 0 && productsSum < freeShippingMinSum){
+    else if(productsSum > 0 && productsSum < freeShippingMinSum){
     shippingSum = shippingPrice;
 }
 
